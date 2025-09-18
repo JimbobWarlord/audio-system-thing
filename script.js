6146961918350807c62e803cb991b6725264e8d8
JimbobWarlord <s4092060@student.rmit.edu.au>
2025-09-18 15:50:29 +1000

    update

--- script.js
+++ script.js
@@ -143,3 +143,15 @@ nightmarekingBtn.addEventListener("click", () => {
   audioSelect.volume = 0.25;
   footer.innerHTML = "NOW PLAYING - NIGHTMARE KING";
 });
+
+document.addEventListener("keydown", function (event) {
+  if (event.key.toLowerCase() === "r") {
+    document.body.style.background = "radial-gradient(#782623, black)";
+  }
+});
+
+document.addEventListener("keydown", function (event) {
+  if (event.key.toLowerCase() === "b") {
+    document.body.style.background = "radial-gradient(#253a5c, black)";
+  }
+});
